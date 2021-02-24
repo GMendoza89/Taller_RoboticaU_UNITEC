5bf84f6f302cf5b543fb6518abfe36605b421761
Gustavo David Mendoza Pinto <gustavo.mendoza.dp@gmail.com>
2021-02-23 19:33:27 -0600

    Se agrega memoria dinamica, y se genera archivo de salida

--- C++/Fibonacci.cpp
+++ C++/Fibonacci.cpp
@@ -1,42 +1,55 @@
 #include<iostream>
-//#include<fstream>
+#include<fstream>
+#include<string>
 
 int main(int argc, char** argv){
-
-unsigned int numero_elementos, numero_presente, numero_pasado, numero_futuro,contador;
-if(argc!=2){
+//declaracion de variables
+unsigned int numero_elementos, ext;
+long unsigned int *serie_fibonacci;
+char nombre[20];
+std::ofstream fichero;
+std::string nombre_fichero;
+std::string extencion[2];
+extencion[0]=".txt";
+extencion[1]=".csv";
+ext = 0;
+//lectura de argumentos o solicitud de datos
+if(argc!=4){
     std::cout<<"Favor de ingresar un número: ";
- std::cin>>numero_elementos;}
+    std::cin>>numero_elementos;
+    std::cout<<std::endl;
+    std::cout<<"Ingresar nombre de fichero de salida: ";
+    std::cin>>nombre;
+    std::cout<<"Ingresar la extencion 0 - txt (por defecto) 1 - csv: ";
+    std::cin>>ext;
+    nombre_fichero = std::string(nombre)+extencion[0];
+ }
 else{
-    numero_elementos = atoi(argv[1]);}
+    numero_elementos = atoi(argv[1]);
+    nombre_fichero = std::string(argv[2])+extencion[atoi(argv[3])];}
 std::cout<<std::endl;
-int fibonacci[100];
 
-fibonacci[0] = 0;
-fibonacci[1] = 1;
-numero_pasado = 0;
-numero_presente = 1;
-contador = 1;
-std::cout<<"Serie de Fibonanncy: "<<numero_pasado<<", ";
-//Iniciamos ciclo de impresion
-// while(contador<numero_elementos){
-// std::cout<<numero_presente<<", ";               // mostramos número presente
-// numero_futuro = numero_presente+numero_pasado;  // calculamos el el proximo numero futuro
-// numero_pasado = numero_presente;                // guardamos el numero pasado 
-// numero_presente = numero_futuro;                // guardamos nuevo número presente
-// contador++;                                     // incrementamos contador
-// }
-std::cout<<"Serie de Fibonanncy: "<<fibonacci[0]<<", ";
-while (contador<numero_elementos)
-{
-    std::cout<<fibonacci[contador]<<", ";
-    fibonacci[contador+1] = fibonacci[contador]+fibonacci[contador-1];
-    ++contador;
+serie_fibonacci = new long unsigned int(numero_elementos);
+fichero.open(nombre_fichero.c_str());
+
+serie_fibonacci[0] = 0;
+serie_fibonacci[1] = 1;
+
+std::cout<<"Serie de Fibonanncy: "<<serie_fibonacci[0];
+fichero<<"Serie de fibonacci\n";
+fichero<<serie_fibonacci[0];
+
+for(int i0 = 1; i0<numero_elementos-1;++i0){
+    serie_fibonacci[i0+1] = serie_fibonacci[i0]+serie_fibonacci[i0-1];
+    std::cout<<", "<<serie_fibonacci[i0];
+    fichero<<", "<<serie_fibonacci[i0];
 }
-
+std::cout<<", "<<serie_fibonacci[numero_elementos-1];
+fichero<<", "<<serie_fibonacci[numero_elementos-1];
 std::cout<<std::endl;
-//generar con arreglo dinámico
-    return 0;
+fichero.close();
+delete serie_fibonacci;
+return 0;
 
 }
 
